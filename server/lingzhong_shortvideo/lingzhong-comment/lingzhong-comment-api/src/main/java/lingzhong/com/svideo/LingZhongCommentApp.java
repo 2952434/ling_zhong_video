f4930134a3584ce40ae66cd640ae3128b62e0165
孙铭杰 <1830955960@qq.com>
2023-10-26 23:48:34 +0800

    配置swagger3、数据源、服务注册中心、nacos2.2.3版本

--- server/lingzhong_shortvideo/lingzhong-comment/lingzhong-comment-api/src/main/java/lingzhong/com/svideo/LingZhongCommentApp.java
+++ server/lingzhong_shortvideo/lingzhong-comment/lingzhong-comment-api/src/main/java/lingzhong/com/svideo/LingZhongCommentApp.java
@@ -3,8 +3,7 @@ package lingzhong.com.svideo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import springfox.documentation.oas.annotations.EnableOpenApi;
+
 
 /**
  * @Author 孙铭杰
@@ -13,7 +12,6 @@ import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@CrossOrigin(origins = "http://localhost:9023")
 public class LingZhongCommentApp {
     public static void main(String[] args) {
         SpringApplication.run(LingZhongCommentApp.class , args);
